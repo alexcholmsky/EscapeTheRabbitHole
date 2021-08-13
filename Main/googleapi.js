b99c420f2a89134a2fb22098e40ac483288cd4b2
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2021-08-13 12:01:17 -0400

    Code commit

--- Main/googleapi.js
+++ Main/googleapi.js
@@ -1,20 +1,21 @@
-function hndlr(response) {
-    for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
-        // in production code, item.htmlTitle should have the HTML entities escaped.
-        document.getElementById("content").innerHTML += item.title + "<br>"
-        document.getElementById("content").innerHTML += item.link + "<br><br>";
+var count = 0;
+var keyword = "trump news"
+const sources = ['www.bloomberg.com','www.nbcnews.com','www.cnn.com','www.theguardian.com','www.nytimes.com','www.theamericanconservative.com','www.washingtontimes.com','www.foxnews.com','www.breitbart.com','www.nationalreview.com'];
+const link = ['#bloomberg','#nbc','#cnn','#theguardian','#nytimes','#theamericanconservative','#washingtontimes','#foxnews','#breitbart','#nationalreview'];
+
+
+while(count < 11){
+    var fullSrc = "https://www.googleapis.com/customsearch/v1?siteSearch="+sources[count]+"&siteSearchFilter=i&key=AIzaSyDb54oEYNtY2zKGeyC7cqJGBP7t1VBCrk4&cx=6adbb15b85973bdf7&q="+keyword+"&callback=hndlr&num=1"
+    $(link[count]).attr("src",fullSrc);
+
+    function hndlr(response) {
+        for (var i = 0; i < response.items.length; i++) {
+            var item = response.items[i];
+            // in production code, item.htmlTitle should have the HTML entities escaped.
+            document.getElementById("content").innerHTML += item.title + "<br>"
+            document.getElementById("content").innerHTML += item.link + "<br><br>";
+        }
     }
+
+    count+=1;
 }
-
-var test = "trump news"
-var fullSrc = "https://www.googleapis.com/customsearch/v1?siteSearch=www.cnn.com&siteSearchFilter=i&key=AIzaSyDb54oEYNtY2zKGeyC7cqJGBP7t1VBCrk4&cx=6adbb15b85973bdf7&q="+test+"&callback=hndlr&num=1"
-$('#javaone').attr("src",fullSrc);
-
-var test2 = "trump news"
-var fullSrc = "https://www.googleapis.com/customsearch/v1?siteSearch=www.foxnews.com&siteSearchFilter=i&key=AIzaSyA-V7Mm3NUtf5zOutpiSaYZk7sZPHm6VSQ&cx=6adbb15b85973bdf7&q="+test2+"&callback=hndlr&num=1"
-$('#javatwo').attr("src",fullSrc);
-
-var test2 = "trump news"
-var fullSrc = "https://www.googleapis.com/customsearch/v1?siteSearch=www.nbc.com&siteSearchFilter=i&key=AIzaSyA-V7Mm3NUtf5zOutpiSaYZk7sZPHm6VSQ&cx=6adbb15b85973bdf7&q="+test2+"&callback=hndlr&num=1"
-$('#javathree').attr("src",fullSrc);
--- Main/webscraper.py
+++ Main/webscraper.py
@@ -1,9 +1,8 @@
-import parameters
-import selenium
-from time import sleep
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import openpyxl as xl
 import re
 # setup
