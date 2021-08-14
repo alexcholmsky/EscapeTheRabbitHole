7a64efc713ae87b647cf588a969f58190d657c82
jasondu7297 <78238426+jasondu7297@users.noreply.github.com>
2021-08-14 19:19:02 -0400

    New commits

--- Main/master.js
+++ Main/master.js
@@ -1,22 +1,7 @@
-let doneHighlightButton = document.getElementById("changeColor");
-
-function getSelectionText() {
-  var text = "";
-  if (window.getSelection) {
-    text = window.getSelection().toString();
-    alert(text);
-    return text;
-  }
+function getPastedText() {
+  let inputbox = document.getElementById('input').value
+  return inputbox
 }
-
-function hidingInstructions() {
-  var inst = document.getElementById("instructions");
-  if (inst.style.display === "block") {
-    inst.style.display = "none";
-  }
-}
-
-let button = document.getElementById('testing')
 
 const sendData = async (text) => {
   try {
@@ -49,28 +34,18 @@ const sendData = async (text) => {
 
 const processSelected = async () => {
   // 1) get selected text
-  let test = getSelectionText();
-  let test2 = "The ring ouzel (Turdus torquatus) is a medium-sized thrush, breeding mainly in Europe. Males are mostly black with a white crescent across the breast, females are browner and duller than males, and young birds may lack chest markings."
-  console.log(test);
-  alert(test);
+  let test = getPastedText(); 
 
   // 2) get keywords for the text
-  let res = await sendData(test2);
+  let res = await sendData(test);
   console.log(res.keywords[0]);
-  alert(res);
 
   // 3) call function to get articles (googleapi.js)
   const word = res.keywords[0];
   let results = googleapi(word);
   console.log(results);
-  alert(results);
 
   // 4) display articles to UI
   let articles = hndlr(results)
   console.log(articles);
-  alert(articles);
 }
-
-button.addEventListener('click', () => {
-  processSelected();
-})
